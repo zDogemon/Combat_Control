3b8a60dcb35f02aa13ac907196dcfee83f997b40
zDogemon <brunowuklehm@gmail.com>
2019-06-04 21:40:26 -0300

    Icones alterados

--- app/src/main/java/com/catolicasc/combatcontrol/CombatesFragment.java
+++ app/src/main/java/com/catolicasc/combatcontrol/CombatesFragment.java
@@ -7,22 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class TesteFragment extends Fragment {
-
-    TextView texto;
+public class CombatesFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_teste, container, false);
+        return inflater.inflate(R.layout.fragment_combates, container, false);
     }
 
-//    @Override
-//    public void onCreate(@Nullable Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        //texto.findViewById(R.id.txtTeste);
-//        //texto.setText("Queijo Ralado");
-//    }
 }
