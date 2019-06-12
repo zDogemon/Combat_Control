c67e2c610e0a2bdf99e1de27196af039b696b277
douglas.wagner <douglas.wagner@catolicasc.org.br>
2019-06-11 21:40:04 -0300

    -Tela Cadastro -Tela lobby

--- app/src/main/java/com/catolicasc/combatcontrol/LoginActivity.java
+++ app/src/main/java/com/catolicasc/combatcontrol/LoginActivity.java
@@ -1,14 +1,50 @@
 package com.catolicasc.combatcontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnCadastrar;
+    private Button btnLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //to-do
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity_Register();
+            }
+        });
+
+       btnLogar = findViewById(R.id.btnLogar);
+       btnLogar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openActivity_Login();
+           }
+       });
+    }
+
+
+    public void openActivity_Register(){
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void openActivity_Login(){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
